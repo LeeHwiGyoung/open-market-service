44cc895a7a67380db78cdff17ac49669ba0044c3
HwiGyoung Lee <ter0000@naver.com>
2024-10-13 21:03:20 +0900

    ✨login.js id,password에 keyup 이벤트 추가

--- src/js/login.js
+++ src/js/login.js
@@ -2,8 +2,15 @@ const login_form = document.getElementsByName("login")[0];
 const login_type_container = login_form.querySelector(
   ".login-select-container"
 );
-let login_type = "buyer";
+const input_id = login_form.querySelector(".input-login-id");
+const input_password = login_form.querySelector(".input-login-password");
+const msg = login_form.querySelector(".msg");
 
+let login_type = "BUYER";
+let user_name = "";
+let password = "";
+
+async function postLogin() {}
 //login_type 을 선택하는 이벤트
 login_type_container.addEventListener("click", (e) => {
   if (e.target.nodeName === "BUTTON") {
@@ -21,4 +28,12 @@ login_type_container.addEventListener("click", (e) => {
 
 login_form.addEventListener("submit", (e) => {
   e.preventDefault();
+});
+
+input_id.addEventListener("keyup", (e) => {
+  user_name = e.target.value;
+});
+
+input_password.addEventListener("keyup", (e) => {
+  password = e.target.value;
 });

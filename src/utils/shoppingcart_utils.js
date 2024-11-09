0bf0c6678764dc5bba0a4a008fbcab268154ec98
HwiGyoung Lee <ter0000@naver.com>
2024-11-09 16:48:33 +0900

    ✨ 장바구니의 특정 아이템 정보 갖고 오는 기능 및 특정 아이템 수정 기능 추가

--- src/utils/shoppingcart_utils.js
+++ src/utils/shoppingcart_utils.js
@@ -18,3 +18,44 @@ export const deleteShoppingItem = async (url) => {
     console.error(err);
   }
 };
+
+export const getShoppingItem = async (url, id) => {
+  try {
+    const token = get_access_token();
+    const res = await fetch(`${BASE_URL}${url}${id}`, {
+      method: "get",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    if (!res.ok) {
+      throw new Error("netWork Err", res.status);
+    }
+    const json = await res.json();
+    return json;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const updateShoppingItemQunantity = async (url, id, body) => {
+  try {
+    const token = get_access_token();
+    const res = await fetch(`${BASE_URL}${url}${id}`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    if (!res.ok) {
+      throw new Error("netWork Err", res.status);
+    }
+    const json = await res.json();
+    return json;
+  } catch (err) {
+    console.error(err);
+  }
+};

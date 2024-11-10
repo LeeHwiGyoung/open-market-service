ebeac983247f57eb9c8436913e89a51184c909d8
HwiGyoung Lee <ter0000@naver.com>
2024-11-10 15:18:25 +0900

    ✨ sessionStorage를 통하여 페이지가 변해도 클로저를 기억하는 기능 추가

--- src/js/selectedCartItem.js
+++ src/js/selectedCartItem.js
@@ -1,19 +1,28 @@
 function selectedCartItem() {
-  const selectItem = new Set();
+  const selectItem = new Set(
+    JSON.parse(sessionStorage.getItem("cartItems")) || []
+  );
+
   return {
     addItem(id) {
       selectItem.add(id);
+      sessionStorage.setItem("cartItems", JSON.stringify([...selectItem]));
     },
 
     removeItem(id) {
       selectItem.delete(id);
+      sessionStorage.setItem("cartItems", JSON.stringify([...selectItem]));
     },
+
     has(id) {
       return selectItem.has(id);
     },
+
     clearCartList() {
       selectItem.clear();
+      sessionStorage.removeItem("cartItems");
     },
+
     getCartList() {
       return [...selectItem];
     },
